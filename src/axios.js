5e38ac928443d8d74059b7bac02e16df970c06c8
Aryannn959 <Aryannn959>
2026-04-07 15:26:54 +0530

    updated api base url for production

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-project-8f4c3/us-central1/api"
+   baseURL: "https://us-central1-clone-project-8f4c3.cloudfunctions.net/api"
 });
 
 export default instance;
